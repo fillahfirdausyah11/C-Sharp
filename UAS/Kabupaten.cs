720b26227333bd998b86be8083d7504ea5fbaec3
firdausyah22 <fillahfirdausyah22@gmail.com>
2020-07-08 13:32:58 +0700

    Fourth Commit

--- UAS/Kabupaten.cs
+++ UAS/Kabupaten.cs
@@ -6,10 +6,7 @@ namespace dataProvinsi {
 
     class Kabupaten : Yogyakarta {
         public void kecamatan() {
-            Console.WriteLine("1.Kecamatan di Sleman:");
-            Console.WriteLine("Condong Caatur");
-            Console.WriteLine("Wedomartani:");
-            Console.WriteLine("Purowartani:");
+            // Kecamatan disini
         }
     }
 
