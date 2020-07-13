3bc06ea375d8a586c9c45d72f147ff837fcac3f1
fajarabdillah <fajarabdillah72@gmail.com>
2020-07-13 03:36:20 -0700

    ngedit dikit

--- UAS/Kabupaten.cs
+++ UAS/Kabupaten.cs
@@ -7,23 +7,22 @@ namespace dataProvinsi {
     class Kabupaten : Yogyakarta {
         public void kecamatan() {
             // Kecamatan disini
-            Console.WriteLine("Kecamatan di DIY :");
-            Console.WriteLine("Kecamatan DANUREJAN ");
-            Console.WriteLine("Kecamatan GEDONGTEGEN");
-            Console.WriteLine("Kecamatan GONDOKUSUMA");
-            Console.WriteLine("Kecamatan GONDOMANAN");
-            Console.WriteLine("Kecamatan JETIS");
-            Console.WriteLine("Kecamatan KOTAGEDE");
-            Console.WriteLine("Kecamatan KRATON");
-            Console.WriteLine("Kecamatan MANTRIJERON");
-            Console.WriteLine("Kecamatan MERGANGSAN");
-            Console.WriteLine("Kecamatan NGAMPILAN");
-            Console.WriteLine("Kecamatan PAKUALAMAN");
-            Console.WriteLine("Kecamatan TEGALREJO");
-            Console.WriteLine("Kecamatan UMBULHARJO");
-            Console.WriteLine("Kecamatan WIROBRAJAN");
+            Console.WriteLine("\n Kecamatan di DIY :");
+            Console.WriteLine(" 1.  Kecamatan DANUREJAN ");
+            Console.WriteLine(" 2.  Kecamatan GEDONGTEGEN");
+            Console.WriteLine(" 3.  Kecamatan GONDOKUSUMA");
+            Console.WriteLine(" 4.  Kecamatan GONDOMANAN");
+            Console.WriteLine(" 5.  Kecamatan JETIS");
+            Console.WriteLine(" 6.  Kecamatan KOTAGEDE");
+            Console.WriteLine(" 7.  Kecamatan KRATON");
+            Console.WriteLine(" 8.  Kecamatan MANTRIJERON");
+            Console.WriteLine(" 9.  Kecamatan MERGANGSAN");
+            Console.WriteLine(" 10. Kecamatan NGAMPILAN");
+            Console.WriteLine(" 11. Kecamatan PAKUALAMAN");
+            Console.WriteLine(" 12. Kecamatan TEGALREJO");
+            Console.WriteLine(" 13. Kecamatan UMBULHARJO");
+            Console.WriteLine(" 14. Kecamatan WIROBRAJAN");
 
         }
     }
-
 }
